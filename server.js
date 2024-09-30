09615bfa5586ff2e2d3a7730595a306316b74791
bjrfx <phanikiran1234@gmail.com>
2024-09-29 23:10:19 -0400

    v1.0

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5001;
+
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle all other requests by serving the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
